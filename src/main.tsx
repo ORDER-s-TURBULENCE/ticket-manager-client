92811d471285add544ae37a604ef96e89683369a
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2025-12-10 01:10:55 +0900

    feat: Add QueryClientProvider to main entry

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { Provider } from './components/ui/provider.tsx'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './routes/root.ts'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <RouterProvider router={router} />
-      {/* <TanStackRouterDevtools router={router} /> */}
+      <QueryClientProvider client={new QueryClient()}>
+        <RouterProvider router={router} />
+        {/* <TanStackRouterDevtools router={router} /> */}
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 )
